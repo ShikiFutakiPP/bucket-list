591d5f504999e874fe104375a986ed4b70e6e745
shiki futaki pp <100916708+ShikiFutakiPP@users.noreply.github.com>
2023-05-02 18:04:25 +0200

    sql

--- idea.sql
+++ idea.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : mar. 02 mai 2023 à 15:11
+-- Généré le : mar. 02 mai 2023 à 16:03
 -- Version du serveur : 5.7.36
 -- Version de PHP : 7.4.26
 
@@ -20,6 +20,53 @@ SET time_zone = "+00:00";
 --
 -- Base de données : `idea`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `app_user`
+--
+
+DROP TABLE IF EXISTS `app_user`;
+CREATE TABLE IF NOT EXISTS `app_user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `date_created` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `app_user`
+--
+
+INSERT INTO `app_user` (`id`, `username`, `email`, `password`, `date_created`) VALUES
+(1, 'test', 'test@gmail.com', '$2y$13$MwXM..QvDc2Vg0F0e/p.oufBpBYlfFM4bH87gfOWXXqakg/qkrQjy', '2022-12-07 12:32:44');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `category`
+--
+
+DROP TABLE IF EXISTS `category`;
+CREATE TABLE IF NOT EXISTS `category` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(25) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `category`
+--
+
+INSERT INTO `category` (`id`, `name`) VALUES
+(1, 'Travel & Adventure'),
+(2, 'Sport'),
+(3, 'Entertainment'),
+(4, 'Human Relations'),
+(5, 'Others');
 
 -- --------------------------------------------------------
 
